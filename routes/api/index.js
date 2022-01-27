9b1bd1a9e3441d154a518d251fd85781e031eb17
bryonbsmb1 <87393401+bryonbsmb1@users.noreply.github.com>
2022-01-27 11:39:21 -0500

    uploaded starter code again to try and fix

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,4 @@
+// re imported starter code to try and fix 
 const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
+const { Model } = require('sequelize');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
-  Product.findAll(
-    {
+  try {
+    const productData = await Product.findAll({
+      // be sure to include its associated Category and Tag data
       include: [
         {
           model: Category,
@@ -17,44 +18,54 @@ router.get('/', (req, res) => {
         {
           model: Tag,
           attributes: ['tag_name']
-        }
-      ]
-    }
-  )
-    .then(productData => res.json(productData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+        }]
     });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  Product.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [{
-      model: Category,
-      attributes: ['category_name']
-    },
-    {
-      model: Tag,
-      attributes: ['tag_name']
+  try {
+    const categoryData = await Product.findByPk(req.params.id,
+      {
+        include: [
+          {
+            model: Category,
+            attributes: ['category_name']
+          },
+          {
+            model: Tag,
+            attributes: ['tag_name']
+          }
+        ]
+      }
+    );
+    if (!categoryData) {
+      res.status(404).json({ message: 'no category found with this ID' });
+      return;
     }
-    ]
-  })
-    .then(productData => res.json(productData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -78,7 +89,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -96,10 +107,10 @@ router.put('/:id', (req, res) => {
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
-          // return {
-          //   product_id: req.params.id,
-          //   tag_id,
-          // };
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
         });
       // figure out which ones to remove
       const productTagsToRemove = productTags
@@ -119,24 +130,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(productData => {
-      if (!productData) {
-        res.status(404).json({ message: 'No Product found with that ID.' });
-        return;
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
       }
-      res.json(productData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
     });
+
+    if (!productData){
+      res.status(404).json({ message: 'no product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
